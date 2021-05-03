89a2ad4721e4db533fdc23b5405a37731e125e44
Ilhomjon <IlhomjonIbragimov1502@gmail.com>
2021-05-03 14:13:31 +0500

    Memory game test

--- app/src/main/java/com/ilhomjon/testmemorygame/MainActivity.kt
+++ app/src/main/java/com/ilhomjon/testmemorygame/MainActivity.kt
@@ -9,106 +9,135 @@ import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    var listImageOchiqYOpiq = arrayOf(false, false, false, false, false, false)
-    var rasmIndex = arrayOfNulls<Int>(2)
-    var rasmId = arrayOfNulls<Int>(2)
+
+    val listImageOchiqYopiq = arrayOf(false, false, false, false, false, false)
+    val imageIndex = arrayOfNulls<Int>(2)
+    val rasmId = arrayOfNulls<Int>(2)
     var ochiqRasm = 0
+    var animationDoing = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         image_1.setOnClickListener {
-            btnClick(image_1, R.drawable.moto_rasm, 0)
+            imageClick(image_1, R.drawable.moto_rasm, 0)
         }
         image_2.setOnClickListener {
-            btnClick(image_2, R.drawable.moto_rasm, 1)
+            imageClick(image_2, R.drawable.tabiat_rasm_2, 1)
+        }
+        image_3.setOnClickListener {
+            imageClick(image_3, R.drawable.tabiat_rasm_3, 2)
+        }
+        image_4.setOnClickListener {
+            imageClick(image_4, R.drawable.moto_rasm, 3)
+        }
+        image_5.setOnClickListener {
+            imageClick(image_5, R.drawable.tabiat_rasm_2, 4)
+        }
+        image_6.setOnClickListener {
+            imageClick(image_6, R.drawable.tabiat_rasm_3, 5)
         }
     }
 
-    fun btnClick(imageView: ImageView, rasm:Int, index:Int){
-        if (listImageOchiqYOpiq[index] == false) {
-            animationOchilishi(rasm, imageView, index)
-            listImageOchiqYOpiq[index] = true
-        } else {
-            animationYopilishi(rasm, imageView, index)
-
+    fun imageClick(imageView: ImageView, rasm: Int, index:Int){
+        if (!animationDoing) {
+            if (listImageOchiqYopiq[index] == false) {
+                animationOchilishi(imageView, rasm, index)
+            } else {
+                animationYopilishi(imageView, rasm, index)
+            }
         }
     }
-
-    fun animationOchilishi(rasm:Int, imageView:ImageView, index: Int){
-        val animation = AnimationUtils.loadAnimation(this, R.anim.anim_1)
-        animation.setAnimationListener(object : Animation.AnimationListener {
+    fun animationOchilishi(imageView: ImageView, rasm:Int, index: Int){
+        val animation= AnimationUtils.loadAnimation(this, R.anim.anim_1)
+        imageView.startAnimation(animation)
+        animation.setAnimationListener(object : Animation.AnimationListener{
             override fun onAnimationStart(animation: Animation?) {
-
+                animationDoing = true
             }
 
             override fun onAnimationEnd(animation: Animation?) {
                 val animation2 = AnimationUtils.loadAnimation(this@MainActivity, R.anim.anim_2)
                 imageView.startAnimation(animation2)
                 imageView.setImageResource(rasm)
-                animation2.setAnimationListener(object :Animation.AnimationListener{
+                animation2.setAnimationListener(object : Animation.AnimationListener{
                     override fun onAnimationStart(animation: Animation?) {
 
                     }
 
                     override fun onAnimationEnd(animation: Animation?) {
-                        println(ochiqRasm)
-                        rasmIndex[ochiqRasm] = index
+                        listImageOchiqYopiq[index] = true
+                        imageIndex[ochiqRasm] = index
                         rasmId[ochiqRasm] = rasm
                         ochiqRasm++
-                        if (ochiqRasm == 2) {
-                            if (rasmId[0] == rasmId[1]) {
-                                imageViewAniqla(rasmIndex[0]).visibility = View.INVISIBLE
-                                imageViewAniqla(rasmIndex[1]).visibility = View.INVISIBLE
-                            } else {
-                                animationYopilishi(-1, imageViewAniqla(rasmIndex[0]), rasmIndex[0])
-                                animationYopilishi(-1, imageViewAniqla(rasmIndex[1]), rasmIndex[1])
 
+                        if (ochiqRasm == 2){
+                            if (rasmId[0] == rasmId[1]){
+                                imageViewAniqla(imageIndex[0]).visibility = View.INVISIBLE
+                                ochiqRasm--
+                                imageViewAniqla(imageIndex[1]).visibility = View.INVISIBLE
+                                ochiqRasm--
+                            }else{
+                                animationYopilishi(imageViewAniqla(imageIndex[0]), -1, imageIndex[0])
+                                animationYopilishi(imageViewAniqla(imageIndex[1]), -1, imageIndex[1])
                             }
-                            rasmIndex[0] = null
-                            rasmIndex[1] = null
                         }
+                        animationDoing = false
                     }
 
                     override fun onAnimationRepeat(animation: Animation?) {
 
                     }
                 })
-
             }
 
             override fun onAnimationRepeat(animation: Animation?) {
 
             }
         })
-        imageView.startAnimation(animation)
 
     }
 
-    fun animationYopilishi(rasm: Int, imageView: ImageView, index: Int?){
-        val animation = AnimationUtils.loadAnimation(this, R.anim.anim_1)
+
+
+    fun animationYopilishi(imageView: ImageView, rasm:Int, index: Int?){
+        val animation= AnimationUtils.loadAnimation(this, R.anim.anim_1)
+        imageView.startAnimation(animation)
         animation.setAnimationListener(object : Animation.AnimationListener{
             override fun onAnimationStart(animation: Animation?) {
-
+                animationDoing = true
             }
 
             override fun onAnimationEnd(animation: Animation?) {
                 val animation2 = AnimationUtils.loadAnimation(this@MainActivity, R.anim.anim_2)
                 imageView.startAnimation(animation2)
                 imageView.setImageResource(R.drawable.yulduzcha)
+                animation2.setAnimationListener(object : Animation.AnimationListener{
+                    override fun onAnimationStart(animation: Animation?) {
+
+                    }
+
+                    override fun onAnimationEnd(animation: Animation?) {
+                        animationDoing = false
+                    }
+
+                    override fun onAnimationRepeat(animation: Animation?) {
+
+                    }
+                })
             }
 
             override fun onAnimationRepeat(animation: Animation?) {
 
             }
         })
-        imageView.startAnimation(animation)
-         ochiqRasm--
-        listImageOchiqYOpiq[index!!] = false
+        listImageOchiqYopiq[index!!] = false
+        ochiqRasm--
     }
 
     fun imageViewAniqla(index: Int?):ImageView{
-        var imageView:ImageView? = null
+        var imageView:ImageView? =null
         when(index){
             0 -> imageView = image_1
             1 -> imageView = image_2
@@ -119,4 +148,5 @@ class MainActivity : AppCompatActivity() {
         }
         return imageView!!
     }
+
 }
